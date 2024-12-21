630cbe7ff765c4888fc3101d464c23ff69cbdec8
Igor Henrique Koga Vigato <ihenriquekogavigato@gmail.com>
2024-12-20 21:21:30 -0300

    Atualização

--- Aplicacao de Programacao Funcional/FuncaoMap.js
+++ Aplicacao de Programacao Funcional/FuncaoMap.js
@@ -1,9 +1,13 @@
+//Função map
+//declarando função que dobra o valor de um numero
 function dobro(number){
     return number * 2;
 };
 
+//Uma lista de numeros
 var numeros = [33, 4, 5, 9, 22, 44];
 
+//utilizando o comando map para mandar os numeros da lista para a função
 var resultado = numeros.map(dobro);
 
 console.log(resultado);
